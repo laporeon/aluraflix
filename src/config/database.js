49c46561bb35422673ab5458989b1cf11b670d5e
Laíse Carvalho <34722707+laporeon@users.noreply.github.com>
2022-01-19 19:07:25 -0300

    feat: create database config files

---
+++ src/config/database.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+module.exports = {
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB,
+  port: process.env.DB_PORT,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+  },
+  dialectOptions: {
+    useUTC: false,
+  },
+  timezone: "-03:00",
+};
