ae2079a66bdd753ba84dd0ca623304a3c8d07505
Laíse Carvalho <34722707+laporeon@users.noreply.github.com>
2022-01-24 16:22:04 -0300

    feat: create all migrations

---
+++ src/database/migrations/20220124172723-create-categories-table.js
@@ -0,0 +1,36 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("categories", {
+      id: {
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+        type: Sequelize.UUID,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      color: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        required: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("categories");
+  },
+};
