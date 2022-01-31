dd4615dcca6a3062f2be56adb1be72baa0c26f86
Laíse Carvalho <34722707+laporeon@users.noreply.github.com>
2022-01-31 18:52:09 -0300

    refactor: change names of methods

--- src/controllers/CategoryController.js
+++ src/controllers/CategoryController.js
@@ -16,7 +16,7 @@ class CategoryController {
     }
   }
 
-  async getAll(req, res) {
+  async list(req, res) {
     try {
       const categories = await Category.findAll({
         attributes: ["id", "title", "color"],
@@ -31,10 +31,13 @@ class CategoryController {
     }
   }
 
-  async getOneById(req, res) {
+  async getById(req, res) {
     try {
       const { id } = req.params;
       const category = await Category.findByPk(id);
+
+      if (!category)
+        return res.status(404).json({ error: "Category not found" });
 
       return res.status(200).json({ category });
     } catch (error) {
