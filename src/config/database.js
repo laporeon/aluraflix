aec37c7cc71138dd69a953dfa7056c85827ace29
Laíse Carvalho <34722707+laporeon@users.noreply.github.com>
2022-02-01 20:38:43 -0300

    fix: revert changes

--- src/config/database.js
+++ src/config/database.js
@@ -1,27 +1,21 @@
 require("dotenv").config();
 
 module.exports = {
-  development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    port: process.env.DB_PORT,
-    define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true,
-      createdAt: "created_at",
-      updatedAt: "updated_at",
-    },
-    dialectOptions: {
-      useUTC: false,
-    },
-    timezone: "-03:00",
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB,
+  port: process.env.DB_PORT,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+    createdAt: "created_at",
+    updatedAt: "updated_at",
   },
-  production: {
-    use_env_variable: "CLEARDB_DATABASE_URL",
-    dialect: "mysql",
+  dialectOptions: {
+    useUTC: false,
   },
+  timezone: "-03:00",
 };
