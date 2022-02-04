bd45cb9709c96b7e1338ad15666903c800f6b181
Laíse Carvalho <34722707+laporeon@users.noreply.github.com>
2022-02-04 17:21:14 -0300

    fix(migration): change onDelete to CASCADE

--- src/database/migrations/20220125164919-create-videos-table.js
+++ src/database/migrations/20220125164919-create-videos-table.js
@@ -27,7 +27,7 @@ module.exports = {
           key: "id",
         },
         onUpdate: "CASCADE",
-        onDelete: "SET NULL",
+        onDelete: "CASCADE",
       },
       created_at: {
         type: Sequelize.DATE,
