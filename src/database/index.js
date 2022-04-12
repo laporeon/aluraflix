6e12f724c03d1b91cd84caf6226725925ae49274
Laise Carvalho <34722707+laporeon@users.noreply.github.com>
2022-04-12 19:00:33 -0300

    refactor: remove production config

--- src/database/index.js
+++ src/database/index.js
@@ -1,26 +1,12 @@
 const { Sequelize } = require("sequelize");
 
+const databaseConfig = require("../config/database");
 const User = require("../models/User");
 const Category = require("../models/Category");
 const Video = require("../models/Video");
 
-const env = process.env.NODE_ENV || "development";
-const databaseConfig = require(__dirname + "/../config/database.js")[env];
 
-let connection;
-if (databaseConfig.use_env_variable) {
-  connection = new Sequelize(
-    process.env[databaseConfig.use_env_variable],
-    databaseConfig
-  );
-} else {
-  connection = new Sequelize(
-    databaseConfig.database,
-    databaseConfig.username,
-    databaseConfig.password,
-    databaseConfig
-  );
-}
+const connection = new Sequelize(databaseConfig);
 
 const models = [User, Category, Video];
 
